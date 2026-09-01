bd1a30a457c434f63617c18ed43e38329b9fd186
Andro Yousery <androyouseryanower.com>
2026-09-01 12:03:27 +0300

    task session

---
+++ session13/js/script.js
@@ -0,0 +1,54 @@
+
+
+// var myForm = document.querySelector('#myForm');
+// var users = []
+// myForm.addEventListener('submit' , (e)=>{
+//     e.preventDefault();
+//     console.log(e)
+//     console.log(e.target.elements.email.value)
+//     console.log(e.target.elements.name.value)
+//     var user = {
+//         name: e.target.elements.name.value,
+//         email: e.target.elements.email.value,
+//     }
+//     users.push(user)
+//     console.table(users)
+// })
+
+
+
+var form = document.getElementById('myForm');
+
+form.addEventListener('submit', function (event) {
+
+    event.preventDefault();
+
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+
+    var div = document.createElement('div');
+    var h3 = document.createElement('h3');
+    var h4 = document.createElement('h4');
+
+    h3.innerText = name;
+    h4.innerText = email;
+
+    div.appendChild(h3);
+    div.appendChild(h4);
+
+    div.classList.add('alert', 'alert-primary');
+
+    var divContainer = document.getElementById('divContainer');
+
+    divContainer.appendChild(div);
+
+    console.log(div);
+    console.log(h3);
+    console.log(h4);
+});
+
+
+
+
+
+
